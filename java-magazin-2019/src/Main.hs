19c07e864ddf94d5d2ac4dd6cc78b254abdd41a0
Peter Thiemann <peter.thiemann@gmail.com>
2021-09-03 11:33:14 +0200

    refactoring

--- java-magazin-2019/src/Main.hs
+++ java-magazin-2019/src/Main.hs
@@ -1,7 +1,19 @@
 module Main where
 
-import qualified Gameplay
+import qualified Gameplay as G
+import qualified Shuffle
+import qualified Cards
 
 main :: IO ()
-main = Gameplay.start
+main = start
+
+playerMike = G.makePlayer "Mike" G.playAlongStrategy
+playerPeter = G.makePlayer "Peter" G.playInteractive
+playerAnnette = G.makePlayer "Annette" G.playAlongStrategy
+playerNicole = G.makePlayer "Nicole" G.shootTheMoonStrategy
+
+start :: IO ()
+start =
+  G.runGame (Shuffle.shuffleRounds 10 Cards.deck)
+            [playerNicole, playerAnnette, playerPeter, playerMike]
 
